a344416f2d3089812f284255f932ec2cc209bd9c
WEBXICO-MX <weiss.uttab@gmail.com>
2015-12-28 14:51:22 -0600

    Versión inicial.

--- index.php
+++ index.php
@@ -1,17 +1,221 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="es">
     <head>
+        <title>Sitio oficial del H. Ayuntamiento de Macuspana 2016-2018 | Home</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="gobierno, macuspana, 2016,2018, ayuntamiento" />
+        <meta name="description" content="Sitio oficial del H. Ayuntamiento de Macuspana, Tabasco" />
+        <!-- IE -->
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+        <!-- other browsers -->
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+        <link href="twbs/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="css/twbscolor.css" rel="stylesheet"/>
+        <link href="css/municipio1.css" rel="stylesheet"/>
+        <style>
+            .carousel-inner > .item > img,
+            .carousel-inner > .item > a > img {
+                margin: auto;
+            }
+            #google_maps {
+                display: block;
+                width: 100%;
+                height: 200px;
+            }
+        </style>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <img src="img/Logo.gif" alt="" class="img-responsive"/>
+                </div>
+                <div class="col-md-10 text-right ">
+                    <a href="#"><img src="img/rsociales/youtube.png" alt="Youtube"/></a>
+                    <a href="#"><img src="img/rsociales/facebook.png" alt="Facebook"/></a>
+                    <a href="#"><img src="img/rsociales/instagram.png" alt="Instagram"/></a>
+                    <a href="#"><img src="img/rsociales/twitter.png" alt="Twitter"/></a>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-md-12">
+                    <nav class="navbar navbar-default">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                            </div>
+                            <div id="navbar" class="navbar-collapse collapse">
+                                <ul class="nav navbar-nav">
+                                    <li class="active"><a href="index.php">Inicio</a></li>
+                                    <li><a href="#">H. Ayuntamiento</a></li>
+                                    <li><a href="#">Dependencias</a></li>
+                                    <li><a href="#">Trámites</a></li>
+                                    <li><a href="#">Sala de prensa</a></li>
+                                    <li><a href="#">Transparencia</a></li>
+                                </ul>
+                            </div>
+                            <!--/.nav-collapse -->
+                        </div>
+                        <!--/.container-fluid -->
+                    </nav>
+                </div>
+            </div>
+
+            <div class="row" >
+                <div class="col-md-12">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                            <li data-target="#myCarousel" data-slide-to="3"></li>
+                            <li data-target="#myCarousel" data-slide-to="4"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+
+                            <div class="item active">
+                                <img src="Recursos/1.png" alt=""/>
+                                <div class="carousel-caption">
+                                    <h3>Chania</h3>
+                                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="img/slider/img_chania.jpg" alt="Chania" width="460" height="345">
+                                <div class="carousel-caption">
+                                    <h3>Chania</h3>
+                                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <img src="img/slider/img_chania2.jpg" alt="Chania" width="460" height="345">
+                                <div class="carousel-caption">
+                                    <h3>Chania</h3>
+                                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <img src="img/slider/img_flower.jpg" alt="Flower" width="460" height="345">
+                                <div class="carousel-caption">
+                                    <h3>Flowers</h3>
+                                    <p>Beatiful flowers in Kolymbari, Crete.</p>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <img src="img/slider/img_flower2.jpg" alt="Flower" width="460" height="345">
+                                <div class="carousel-caption">
+                                    <h3>Flowers</h3>
+                                    <p>Beatiful flowers in Kolymbari, Crete.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row" id="twitter">
+                <div class="col-md-12">
+                    <h1>Twitter</h1>
+                </div>
+            </div>
+            <div class="row" id="boletines_informativos">
+                <div class="col-md-12">
+                    <h1>Boletines informativos</h1>
+                </div>
+                <div class="col-md-3 text-right">
+                    <img src="Recursos/377.png" alt="Boletin1" class="img-responsive"/>
+                    <p class="text-justify">Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. </p>
+                    <button type="button" class="btn btn-success">Leer más</button>
+                </div>
+                <div class="col-md-3 text-right">
+                    <img src="Recursos/377.png" alt="Boletin1" class="img-responsive"/>
+                    <p class="text-justify">Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. </p>
+                    <button type="button" class="btn btn-success">Leer más</button>
+                </div>
+                <div class="col-md-3 text-right">
+                    <img src="Recursos/377.png" alt="Boletin1" class="img-responsive"/>
+                    <p class="text-justify">Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. </p>
+                    <button type="button" class="btn btn-success">Leer más</button>
+                </div>
+                <div class="col-md-3 text-right">
+                    <img src="Recursos/377.png" alt="Boletin1" class="img-responsive"/>
+                    <p class="text-justify">Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. </p>
+                    <button type="button" class="btn btn-success">Leer más</button>
+                </div>
+                <div class="clearfix visible-md-block"></div>
+            </div>
+            <div class="row top-buffer" id="banners">
+                <div class="col-md-6"><img src="Recursos/btn_academia_CISCO.png" alt="" class="img-responsive" style="float:left;width: 585px; height: 215px;"/></div>
+                <div class="col-md-6"><img src="Recursos/btn_academia_CISCO.png" alt="" class="img-responsive" style="float:right;width: 585px; height: 215px;"/></div>
+                <div class="clearfix visible-md-block"></div>
+                <div class="col-md-3">
+                    <img src="Recursos/btn_proteccion_civil.png" alt="" style="float:left;width: 293px; height: 98px;"/>
+                </div>
+                <div class="col-md-3">
+                    <img src="Recursos/btn_proteccion_civil.png" alt="" class="img-responsive" style="float:left; width: 293px; height: 98px;"/>
+                </div>
+                <div class="col-md-3">
+                    <img src="Recursos/btn_proteccion_civil.png" alt="" class="img-responsive" style="float:right;width: 293px; height: 98px;"/>
+                </div>
+                <div class="col-md-3">
+                    <img src="Recursos/btn_proteccion_civil.png" alt="" class="img-responsive" style="float:right;width: 293px; height: 98px;"/>
+                </div>
+                <div class="clearfix visible-md-block"></div>
+            </div>
+            <div class="row top-buffer" id="contacto">
+                <div class="col-md-12">
+                    <h1>Contacto</h1>
+                </div>
+                <div class="col-md-2">
+                    <img src="img/logoEscudo.png" alt="Gobierno del Estado de Tabasco"/>
+                </div>
+                <div class="col-md-5">
+                    <ul>
+                        <li style="list-style-image: url( img/direccion_ico.png)">Dirección: Por definir</li>
+                        <li style="list-style-image: url( img/telefono_ico.png)">Teléfono: Por definir</li>
+                        <li style="list-style-image: url( img/email_ico.png)">Correo electrónico: Por definir</li>
+                    </ul>
+                </div>
+                <div class="col-md-5">
+                    <div id="google_maps">Cargando Google Maps ...</div>
+                </div>
+                <div class="clearfix visible-md-block"></div>
+            </div>
+            <footer>
+                <div class="row top-buffer">
+                    <div class="col-md-12">
+                        <p class="text-center">Copyright <?php echo date("Y"); ?> &copy; H. Ayuntamiento de Macuspana</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <script src="js/jQuery/jquery-1.11.3.min.js"></script>
+        <script src="twbs/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpdw9gyXrQvIvyLrVi9FneyumQOE8_9CA&sensor=true"></script>
+        <script src="js/maps.js"></script>
     </body>
 </html>
