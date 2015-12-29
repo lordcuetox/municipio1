4c39bfd30fefcdb17181af2586b771a31ab198ab
WEBXICO-MX <weiss.uttab@gmail.com>
2015-12-29 14:53:19 -0600

    Versión inicial.

--- showTweets.php
+++ showTweets.php
@@ -1,15 +1,5 @@
 <?php
 
-    $request_url ="https://api.twitter.com/1/help/test.json";
-    $srvstamp = time();     // ---- server timestamp ---- 
-    $twheader = get_headers($request_url, 1);   // ---- array with ['Date'] string
-    $twistamp = strtotime($twheader['date']);   // -- twitter timestamp
-    echo "
-     Server: ".date("Y.m.d (H:i:s)",$srvstamp)." - ".$srvstamp." \r\n";
-    echo "
-     Twitter: ".date("Y.m.d (H:i:s)",$twistamp)." - ".$twistamp." \r\n";
-    //The diference between my server and twitter is/
-    echo $twistamp - $srvstamp;
         include('clases/TwitterAPIExchange.php');
  
         $settings = array(
@@ -19,7 +9,7 @@
             'consumer_secret' => "8Onn9wuGaxEaRgq9imDo33b4F6zrMRWM55DvULLMo9QL8TnVjz");
  
         $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-        $getfield = '?screen_name=LordCuetox';        
+        $getfield = '?screen_name=LordCuetox&count=1';        
         $requestMethod = 'GET';
         $twitter = new TwitterAPIExchange($settings);
         echo $twitter->setGetfield($getfield)
