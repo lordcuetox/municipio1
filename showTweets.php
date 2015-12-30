bda9bc0f027f58cf2d0baa621d6bf1291acd7a17
lordcuetox <delcueto@gmail.com>
2015-12-30 12:39:35 -0600

    modificando accesos con la cuenta de twitter

--- showTweets.php
+++ showTweets.php
@@ -9,7 +9,7 @@
             'consumer_secret' => "8Onn9wuGaxEaRgq9imDo33b4F6zrMRWM55DvULLMo9QL8TnVjz");
  
         $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-        $getfield = '?screen_name=LordCuetox&count=1';        
+        $getfield = '?screen_name=A_Macuspana&count=1';        
         $requestMethod = 'GET';
         $twitter = new TwitterAPIExchange($settings);
         echo $twitter->setGetfield($getfield)
