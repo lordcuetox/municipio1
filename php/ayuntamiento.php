d81434f7ba4a7019af9ca2d036e187c05b26fd0b
lordcuetox <delcueto@gmail.com>
2016-01-13 13:21:43 -0600

    camb io de imágenes, hoja de estilos y página de ayuntamiento

--- php/ayuntamiento.php
+++ php/ayuntamiento.php
@@ -19,42 +19,57 @@
         <div class="container">
             <?php require_once 'include_header.php'; ?>
             <div class="row">
-                 <div class="col-sm-2">
-                </div>
-                <div class="col-md-10">
+               
+                <div class="col-md-12">
                     <h1 class="text-center">H. Ayuntamiento</h1>
                 </div>
+                     <div class="col-md-12">
                  <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../img/ayuntamiento"/>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/1.png"/>
                     <div>
-                    <div class="puesto">Primer Regidor</div>
+                    <div class="puestoEspecial">Primer Regidor</div>
+                    <div class="puesto2">y Presidente Municipal</div>
                     <div class="nombre">José Eduardo Rovirosa Ramírez</div>
-                </div>
+                </div>              
             </div>
+                <div class="col-sm-2">
+                </div>
+                         </div>
+                  <div class="col-md-12">
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/2.png"/>
                     <div>
-                    <div class="puesto">Segundo Regidor</div>
+                    <div class="puestoEspecial">Segunda Regidora</div>
+                    <div class="puesto2">y Primera Síndico de Hacienda</div>
                     <div class="nombre">Luisa Aurora Sastré Hernández</div>
                 </div>
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/3.png"/>
                     <div>
-                    <div class="puesto">Tercer Regidor</div>
+                    <div class="puestoEspecial">Tercer Regidor</div>
+                     <div class="puesto2">y Segundo Síndico de Hacienda</div>
                     <div class="nombre">Alejandro Priego Pozos</div>
                 </div>
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/4.png"/>
                     <div>
                     <div class="puesto">Cuarto Regidor</div>
                     <div class="nombre">Claudia Dolores Vasconcelos Segura</div>
@@ -62,8 +77,12 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/5.png"/>
                     <div>
                     <div class="puesto">Quinto Regidor</div>
                     <div class="nombre">Felipe Feria Pérez</div>
@@ -71,8 +90,12 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/6.png"/>
                     <div>
                     <div class="puesto">Sexto Regidor</div>
                     <div class="nombre">Rosaura Hernández Jiménez</div>
@@ -80,8 +103,12 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/7.png"/>
                     <div>
                     <div class="puesto">Séptimo Regidor</div>
                     <div class="nombre">Francisco Becerra Ocaña</div>
@@ -89,8 +116,12 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/8.png"/>
                     <div>
                     <div class="puesto">Octavo Regidor</div>
                     <div class="nombre">Guadalupe González Villalobos</div>
@@ -98,8 +129,12 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/9.png"/>
                     <div>
                     <div class="puesto">Noveno Regidor</div>
                     <div class="nombre">Carlos Arias Ramírez</div>
@@ -107,8 +142,12 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/10.png"/>
                     <div>
                     <div class="puesto">Décimo Regidor</div>
                     <div class="nombre">Cinthia Harlet Hernández Morales</div>
@@ -116,8 +155,12 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/11.png"/>
                     <div>
                     <div class="puesto">Décimo Primer Regidor</div>
                     <div class="nombre">Jesús Gabriel Herrera de la Cruz</div>
@@ -125,8 +168,12 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/12.png"/>
                     <div>
                     <div class="puesto">Décimo Segundo Regidor</div>
                     <div class="nombre">José Joaquin Fonz Hernández</div>
@@ -134,8 +181,12 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/13.png"/>
                     <div>
                     <div class="puesto">Décimo Tercer Regidor</div>
                     <div class="nombre">Silvia Yolanda Falcón García</div>
@@ -143,13 +194,20 @@
             </div>
                       <div class="col-sm-2">
                 </div>
-                <div class="col-sm-10 regidores">
-                    <img class="img-responsive" src="../Recursos/users_folder_256.png"/>
+                                   </div>
+                  <div class="col-md-12">
+                      <div class="col-sm-2">
+                </div>
+                <div class="col-sm-8 regidores">
+                    <img class="img-responsive" src="../img/ayuntamiento/14.png"/>
                     <div>
                     <div class="puesto">Décimo Cuarto Regidor</div>
                     <div class="nombre">Mónica Rodríguez Potenciano</div>
                 </div>
             </div>
+                      <div class="col-sm-2">
+                </div>
+                  </div>
             <?php require_once 'include_footer.php'; ?>
         </div>
         <script src="../js/jQuery/jquery-1.11.3.min.js"></script>
