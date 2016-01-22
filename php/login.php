347322240fe6294f068e7416105e1440cc871d2a
lordcuetox <delcueto@gmail.com>
2016-01-22 12:01:11 -0600

    pruebas del login

--- php/login.php
+++ php/login.php
@@ -55,7 +55,7 @@ if (isset($_POST['xAccion'])) {
         <![endif]-->
     </head>
     <body>
-        <?php include 'analyticstracking.php'; ?>
+    
         <div class="container">
             <div class="row">
                 <div class="col-md-4 col-md-offset-4">
