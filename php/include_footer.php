c9d5a5fe9563c8dfc6734149021e8495b6137684
WEBXICO-MX <weiss.uttab@gmail.com>
2016-01-27 16:33:35 -0600

    Se cambio el mapa de google maps a 12 columnas y el texto de los derechos reservados.

--- php/include_footer.php
+++ php/include_footer.php
@@ -11,6 +11,9 @@ if(isset($origin) && $origin != "")
         <h1 class="visible-md visible-lg"> <img src="<?php echo($path);?>img/titulo_mapa.png" alt="" class="img-responsive"/></h1>
         <h2 class="visible-xs visible-sm text-center"><img src="<?php echo($path);?>Recursos/titulo_mapa.png" alt="" class="img-responsive"/></h2>
     </div>
+    <div class="col-md-12">
+        <div id="google_maps">Cargando Google Maps ...</div>
+    </div>
     <div class="col-sm-2 col-md-2">
         <img src="<?php echo($path);?>img/logoEscudo.png" alt="Gobierno del Estado de Tabasco"/>
     </div>
@@ -20,15 +23,13 @@ if(isset($origin) && $origin != "")
             <li style="list-style-image: url( <?php echo($path);?>img/telefono_ico.png)">Teléfono: 01-936-362-0521</li>
         </ul>
     </div>
-    <div class="col-sm-12 col-md-5">
-        <div id="google_maps">Cargando Google Maps ...</div>
-    </div>
+    
     <div class="clearfix visible-md-block"></div>
 </div>
 <footer>
     <div class="row top-buffer">
         <div class="col-md-12">
-            <p class="text-center">Copyright <?php echo date("Y"); ?> &copy; H. Ayuntamiento de Macuspana</p>
+            <p class="text-center">Todos los derechos reservados H. Ayuntamiento de Macuspana, Tabasco, México. Administración 2016-2018</p>
         </div>
     </div>
 </footer>
