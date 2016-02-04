ecec164ff9ae52fc5cc1f667d6c895bb4670357a
WEBXICO-MX <weiss.uttab@gmail.com>
2016-02-04 16:06:25 -0600

    Parametro para saber el nivel de carpetas para la imagen de google map.

--- js/maps.js
+++ js/maps.js
@@ -1,10 +1,12 @@
 var map;
+var param = document.getElementById("map") !== null ? document.getElementById("map").getAttribute("data-name"): "";
+
 function initialize()
 {
     var coordenadas = new google.maps.LatLng(17.7569184,-92.5960686);
     var opt = {zoom: 14, center: coordenadas, mapTypeId: google.maps.MapTypeId.ROADMAP};
     var map = new google.maps.Map(document.getElementById('google_maps'), opt);
-    var marker = new google.maps.Marker({position: coordenadas, animation: google.maps.Animation.DROP, icon: "img/Map-Marker-Push-Pin-1-Left-Pink-icon.png"});
+    var marker = new google.maps.Marker({position: coordenadas, animation: google.maps.Animation.DROP, icon: (param === "" ? "":"../")+"img/Map-Marker-Push-Pin-1-Left-Pink-icon.png"});
     marker.setMap(map);
     var text = "<h3>H. Ayuntamiento de Macuspana 2016-2018</h3>";
     var info = new google.maps.InfoWindow({content: text});
