c041ee4d2af734c2acb65745205677ed5cdc25cd
Invierno <Invierno@Helena>
2016-02-05 05:16:48 -0600

    agregando la pagina de presidencia

---
+++ php/presidencia.php
@@ -0,0 +1,55 @@
+<?php $origin = "presidencia"; ?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Sitio oficial del H. Ayuntamiento de Macuspana 2016-2018 | Presidencia</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="gobierno, macuspana, 2016,2018, ayuntamiento" />
+        <meta name="description" content="Sitio oficial del H. Ayuntamiento de Macuspana, Tabasco. Presidencia" />
+        <!-- IE -->
+        <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico" />
+        <!-- other browsers -->
+        <link rel="icon" type="image/x-icon" href="../img/favicon.ico" />
+        <link href="../twbs/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="../css/twbscolor2.css" rel="stylesheet"/>
+        <link href="../css/municipio1.css" rel="stylesheet"/>
+        <style>
+            .semblanza {
+                font-size: 16px;
+                line-height: 2;
+                background: transparent;
+                 background-repeat: no-repeat;
+                 background-position: bottom;
+                background-image:  url(../img/presidencia/fondo_agua_presidencia-02-color.png);
+                
+            }
+            .semblanza .title {
+                font-size: 18px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <?php require_once 'include_header.php'; ?>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 semblanza">
+                    <img src="../img/ayuntamiento/presid_Foto.jpg" alt="José Eduardo Rovirosa Ramírez" class="img-responsive" style="float:left; margin: 15px 15px 0 0"/>
+                    <img src="../img/ayuntamiento/presid_titulo.jpg" alt="José Eduardo Rovirosa Ramírez" class="img-responsive"/>
+                    <p class="text-justify">Líder y gestor social por naturaleza. Hombre honesto que cumple sus compromisos con hechos y siempre recupera la palabra empeñada. Acostumbrado a convertir las adversidades en oportunidades. Apasionado por resolver los problemas junto a la gente.</p>
+                    <p class="text-justify">Nació en la villa Tepetitán, Macuspana, Tabasco, un 14 de enero de 1955. Felizmente casado con la educadora Crystel Hernández de Rovirosa. Padre de José Eduardo, Lilia Isabel, Eduardo y Crystel.</p>
+                    <p class="text-justify">Su entusiasmo por el campo y las necesidades de éste, lo llevaron a estudiar medicina veterinaria y zootecnia en la Universidad Juárez Autónoma de Tabasco.</p>
+                    <p class="text-justify">Es el tercero de siete hermanos de un matrimonio con sangre comprometida con su pueblo, pues su padre, José Eduardo Rovirosa Carrillo, “Don Pepe Rovirosa”, también fue alcalde de Macuspana de 1980 a 1982. Su madre, doña Ada Ramírez de Rovirosa, fue una mujer altruista muy querida y recordada en su pueblo natal, no en balde la primaria de Tepetitán lleva su nombre.</p>
+                    <p class="text-justify">Su pasión por servir a los demás lo ha llevado a convertirse en el más grande gestor social que ha dado Tabasco, con lo que no solo ha podido ayudar a los más necesitados de su tierra, sino también a miles más a lo largo y ancho de su querido estado.</p>
+                    <p class="text-justify">Por segunda ocasión ocupa el honroso cargo de presidente municipal de su natal Macuspana, para el periodo 2016-2018; la ocasión anterior lo fue en el trienio 2001-2003. Antes, en 1990, le tocó ser  el fundador y, por ende, el primer director del Centro de Capacitación y Reproducción de Especies Menores (Cecarem).</p>
+                    <p class="text-justify">En su carrera partidista ha ocupado los cargos de secretario de gestión social del comité directivo estatal, del Partido Revolucionario Institucional, además de consejero político estatal y municipal.</p>
+                </div>
+            </div>
+            <?php require_once 'include_footer.php'; ?>
+        </div>
+        <script src="../js/jQuery/jquery-1.11.3.min.js"></script>
+        <script src="../twbs/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpdw9gyXrQvIvyLrVi9FneyumQOE8_9CA&sensor=true"></script>
+        <script src="../js/maps.js"></script>
+    </body>
+</html>
