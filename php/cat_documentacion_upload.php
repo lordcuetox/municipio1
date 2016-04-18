cb97f6347d2c6d737576da289a1f263fb00e2a73
WEBXICO-MX <weiss.uttab@gmail.com>
2016-04-18 15:14:24 -0500

    Se valida el $fileType  y el $size.

--- php/cat_documentacion_upload.php
+++ php/cat_documentacion_upload.php
@@ -10,34 +10,41 @@ if (isset($_POST['xAccion2'])) {
 
         $cve_expediente = isset($_POST['xCveExpediente']) ? $_POST['xCveExpediente'] : 0;
         $tipo = isset($_POST["xTipo"]) ? $_POST["xTipo"] : 0;
+
         $target_dir = "../documentos/transparencia/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $fileType = pathinfo($target_file, PATHINFO_EXTENSION);
+        $size = $_FILES["fileToUpload"]["size"];
 
         /* RENOMBRADO DEL ARCHIVO CON LA xCveExpediente */
-        $name_file = basename($_FILES["fileToUpload"]["name"]);
-        $extension = substr($name_file, strpos($name_file, "."), strlen($name_file));
-        $new_name_file = ($tipo == 0) ? ($cve_expediente . "_respuesta" . $extension) : ($tipo == 1 ? ($cve_expediente . "_anexo" . $extension) : ($cve_expediente . $extension));
-        $target_file = $target_dir . $new_name_file;
+        $file_name_new = ($tipo == 0) ? ($cve_expediente . "_respuesta." . $fileType) : ($tipo == 1 ? ($cve_expediente . "_anexo." . $fileType) : ("$cve_expediente.$fileType" ));
+
+        $target_file_new = $target_dir . $file_name_new;
         /* RENOMBRADO DEL ARCHIVO CON LA xCveExpediente */
 
-        if (file_exists($target_file)) {
+        if (file_exists($target_file_new)) {
 
-            unlink($target_file);
+            unlink($target_file_new);
         }
 
-        if ($_FILES["fileToUpload"]["size"] > 500000) {
-            $msg.= "Lo sentimos, su archivo es demasiado grande.\n";
+        if ($fileType != "pdf" && $fileType != "zip" && $fileType != "doc" && $fileType != "docx") {
+            $msg.= "Lo sentimos, solo archivos PDF, DOC, DOCX y ZIP son permitidos. su extensión de archivo es: $fileType";
+            $exito = false;
+        } else if ($size > 10485760) {
+            $msg.= "Lo sentimos, su archivo es demasiado grande.\n El tamaño de su archivo es: " . $size;
             $exito = false;
         } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file_new)) {
                 $msg.= "El archivo " . basename($_FILES["fileToUpload"]["name"]) . " ha sido cargado al servidor.\n";
                 $sql = "";
+                $valor = (substr($target_file_new, 3, strlen($target_file_new)));
 
                 if ($tipo == 0) {
-                    $sql = "UPDATE documentacion_transparencia SET respuesta = '" . (substr($target_file, 3, strlen($target_file))) . "' WHERE cve_expediente = $cve_expediente";
+                    $sql = "UPDATE documentacion_transparencia SET respuesta = '" . $valor . "' WHERE cve_expediente = $cve_expediente";
                 } elseif ($tipo == 1) {
-                    $sql = "UPDATE documentacion_transparencia SET anexo  = '" . (substr($target_file, 3, strlen($target_file))) . "' WHERE cve_expediente = $cve_expediente";
+                    $sql = "UPDATE documentacion_transparencia SET anexo  = '" . $valor . "' WHERE cve_expediente = $cve_expediente";
                 } else {
-                    $sql = "UPDATE documentacion_transparencia SET pdf = '" . (substr($target_file, 3, strlen($target_file))) . "' WHERE cve_expediente = $cve_expediente";
+                    $sql = "UPDATE documentacion_transparencia SET pdf = '" . $valor . "' WHERE cve_expediente = $cve_expediente";
                 }
 
                 $count = UtilDB::ejecutaSQL($sql);
@@ -56,6 +63,7 @@ if (isset($_POST['xAccion2'])) {
         }
         if (!$exito) {
             echo($msg);
+            return;
         } else {
             header('Location:cat_documentacion.php');
             return;
@@ -71,7 +79,6 @@ if (isset($_POST['xAccion2'])) {
     <div class="te">
         <form role="form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data" id="frmUpload" name="frmUpload">
             <div class="form-group">
-                <input type="hidden" name="MAX_FILE_SIZE" value="300000" />
                 <input type="hidden" id="xCveExpediente" name="xCveExpediente" value="<?php echo($_GET["xCveExpediente"]); ?>" />
                 <input type="hidden" id="xTipo" name="xTipo" value="<?php echo($_GET["xTipo"]); ?>" />
                 <input type="hidden" id="xAccion2" name="xAccion2" value="0" />
